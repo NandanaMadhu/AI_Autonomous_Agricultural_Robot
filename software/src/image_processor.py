2297b565ec061c3352381b3b5581b79bd1c05fa2
NandanaMadhu <147269468+NandanaMadhu@users.noreply.github.com>
2026-07-04 14:20:43 +0530

    Update image_processor.py

--- software/src/image_processor.py
+++ software/src/image_processor.py
@@ -1,1 +1,22 @@
+"""
+image_processor.py
 
+Preprocesses captured images before AI inference.
+"""
+
+import cv2
+
+class ImageProcessor:
+
+    def resize(self, image, size=(224,224)):
+        return cv2.resize(image, size)
+
+    def normalize(self, image):
+        return image / 255.0
+
+    def preprocess(self, image):
+
+        image = self.resize(image)
+        image = self.normalize(image)
+
+        return image
