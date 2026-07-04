e89eec0c05e2e93f4d66b754b06981074a7f12b9
NandanaMadhu <147269468+NandanaMadhu@users.noreply.github.com>
2026-07-04 14:21:24 +0530

    Update disease_detector.py

--- software/src/disease_detector.py
+++ software/src/disease_detector.py
@@ -1,1 +1,29 @@
+"""
+disease_detector.py
 
+Loads the TensorFlow model and predicts plant diseases.
+"""
+
+class DiseaseDetector:
+
+    def __init__(self):
+        self.model = None
+
+    def load_model(self):
+
+        print("Loading TensorFlow model...")
+
+        # TODO:
+        # Load .keras model
+
+    def predict(self, image):
+
+        print("Running disease prediction...")
+
+        # TODO
+
+        prediction = "Healthy"
+
+        confidence = 98.5
+
+        return prediction, confidence
