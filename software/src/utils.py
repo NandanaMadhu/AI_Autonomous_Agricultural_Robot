561fe669f18c0f581c71f65266171664080a86a3
NandanaMadhu <147269468+NandanaMadhu@users.noreply.github.com>
2026-07-04 14:24:09 +0530

    Update utils.py

--- software/src/utils.py
+++ software/src/utils.py
@@ -1,1 +1,15 @@
+"""
+utils.py
 
+Utility helper functions.
+"""
+
+import datetime
+
+def get_timestamp():
+
+    return datetime.datetime.now()
+
+def log(message):
+
+    print(f"[INFO] {get_timestamp()} : {message}")
