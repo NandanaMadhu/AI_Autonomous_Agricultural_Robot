a7df002ff7219f596b5296917ea3a7c952a69e50
NandanaMadhu <147269468+NandanaMadhu@users.noreply.github.com>
2026-07-04 14:22:09 +0530

    Update robot_controller.py

--- software/src/robot_controller.py
+++ software/src/robot_controller.py
@@ -1,1 +1,23 @@
+"""
+robot_controller.py
 
+Controls robot movement.
+"""
+
+class RobotController:
+
+    def move_forward(self):
+
+        print("Moving Forward")
+
+    def turn_left(self):
+
+        print("Turning Left")
+
+    def turn_right(self):
+
+        print("Turning Right")
+
+    def stop(self):
+
+        print("Robot Stopped")
