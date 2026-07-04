94b4c7633932e5cc767946f6a39966bf3c391be3
NandanaMadhu <147269468+NandanaMadhu@users.noreply.github.com>
2026-07-04 14:23:40 +0530

    Update obstacle_detection.py

--- software/src/obstacle_detection.py
+++ software/src/obstacle_detection.py
@@ -1,1 +1,17 @@
+"""
+obstacle_detection.py
 
+Processes ultrasonic sensor readings.
+"""
+
+class ObstacleDetection:
+
+    def get_distance(self):
+
+        distance = 45
+
+        return distance
+
+    def is_safe(self, distance):
+
+        return distance > 30
