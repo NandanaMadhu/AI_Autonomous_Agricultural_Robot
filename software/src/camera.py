a6c775643749c55a24b9eeeb083d72f1131a8072
NandanaMadhu <147269468+NandanaMadhu@users.noreply.github.com>
2026-07-04 14:20:06 +0530

    Update camera.py

--- software/src/camera.py
+++ software/src/camera.py
@@ -1,1 +1,23 @@
+"""
+camera.py
 
+Handles image acquisition using the Raspberry Pi Camera Module.
+"""
+
+import cv2
+
+class Camera:
+
+    def __init__(self):
+        self.camera = cv2.VideoCapture(0)
+
+    def capture_frame(self):
+        success, frame = self.camera.read()
+
+        if success:
+            return frame
+
+        return None
+
+    def release(self):
+        self.camera.release()
