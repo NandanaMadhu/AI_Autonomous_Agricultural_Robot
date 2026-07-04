127ad87aecad2f1e3985f4829612717a18276948
NandanaMadhu <147269468+NandanaMadhu@users.noreply.github.com>
2026-07-04 14:22:38 +0530

    Update bluetooth.py

--- software/src/bluetooth.py
+++ software/src/bluetooth.py
@@ -1,1 +1,19 @@
+"""
+bluetooth.py
 
+Handles Bluetooth communication with the mobile application.
+"""
+
+class BluetoothManager:
+
+    def connect(self):
+
+        print("Bluetooth Connected")
+
+    def send_prediction(self, disease):
+
+        print(f"Sending Result : {disease}")
+
+    def disconnect(self):
+
+        print("Bluetooth Disconnected")
